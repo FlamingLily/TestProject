32f5b96549e9bc22af7125f0d754fd67a48af846
FlamingLily <realFlamingLily@gmail.com>
2025-09-04 12:06:12 +1000

    hi

---
+++ Assets/conflict.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class conflict : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("Hi")
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
